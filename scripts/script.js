9c561e96064a0a8d334d3b0bd80da284b3b9cc46
Osmeily <osmeilypernia@gmail.com>
2021-11-16 16:44:08 -0500

    ciclo agregado a const distancia

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,13 @@ let distancia = [6, 3];
 const tamanio = [];
 
 for (let i = 0; i < nombrePlanetas.length; i++) {
-    
+    console.log(nombrePlanetas[i])
+}
+
+for (const key in distancia) {
+    if(distancia[key]==="5"){
+        console.log("Se encontró la distancia")
+    } else {
+        console.log("No se encontró la distancia")
+    }
 }
